7f655dcd23cd0c8f2aa61af651c8ab4dd3a8c585
AndrejK <0502878107@ukr.net>
2022-04-05 12:41:02 +0300

    fix image size in container

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -11,20 +11,20 @@ import RealmSwift
 class MainViewController: UITableViewController {
  
     var places: Results<Place>!
-    var numberOfRows = 0
+   // var numberOfRows = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
         places = realm.objects(Place.self)
-        numberOfRows = places.count
+       // numberOfRows = places.count
     }
 
     // MARK: - Table view data source
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return places.isEmpty ? 0 : numberOfRows
+        return places.isEmpty ? 0 : places.count
     }
 
   
@@ -49,12 +49,12 @@ class MainViewController: UITableViewController {
         let place = places[indexPath.row]
         let action = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (_, _, _) in
             guard let self = self else {return}
-            let alert = UIAlertController(title: "Are you sure?", message: "Delete \(place.name)", preferredStyle: .alert)
+            let alert = UIAlertController(title: "Are you sure?", message: "Delete \(place.name)?", preferredStyle: .alert)
             let ok = UIAlertAction(title: "Ok", style: .default) { (_) in
-                            self.numberOfRows -= 1
+                            //self.numberOfRows -= 1
                             StorageManager.deleteObject(place)
                             self.tableView.deleteRows(at: [indexPath], with: .automatic)
-                            self.tableView.reloadData()
+                            //self.tableView.reloadData()
             }
             let cancel = UIAlertAction(title: "Cancel", style: .destructive) { (_) in
                 self.tableView.reloadData()
@@ -67,17 +67,28 @@ class MainViewController: UITableViewController {
     }
     
     private func editRow(rowIndexPathAt indexPath: IndexPath) -> UIContextualAction {
+        let place = places[indexPath.row]
         let action = UIContextualAction(style: .normal, title: "Edit") { [weak self](_, _, _) in
-            let alert = UIAlertController(title: "Do you want to edit?", message: "You could make some editing of this stuff", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .default))
+            let alert = UIAlertController(title: "Do you want to edit \(place.name)?", message: "You could make some editing of this stuff", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "Ok", style: .default) { (_) in
+                //setupEditScreen()
+               
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .destructive) { (_) in
+                self?.tableView.reloadData()
+            }
+            alert.addAction(ok)
+            alert.addAction(cancel)
+             
             self?.present(alert, animated: true)
         }
         return action
     }
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let edit = self.editRow(rowIndexPathAt: indexPath)
+//       let edit = self.editRow(rowIndexPathAt: indexPath)
         let delete = self.deleteRow(rowIndexPathAt: indexPath)
-        let swipe = UISwipeActionsConfiguration(actions: [delete, edit])
+//         let swipe = UISwipeActionsConfiguration(actions: [delete, edit])
+         let swipe = UISwipeActionsConfiguration(actions: [delete])
         return swipe
     }
 //    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
@@ -93,25 +104,26 @@ class MainViewController: UITableViewController {
 //    }
     
     
-//    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-//        <#code#>
-//    }
-//
-    /*
+ 
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showDetail" {
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            let place = places[indexPath.row]
+            let newPlaceVC = segue.destination as! NewPlaceViewController
+            newPlaceVC.currentPlace = place
+        }
     }
-    */
+    
     
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         
         guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
-        self.numberOfRows += 1
-        newPlaceVC.saveNewPlace()
+        
+         
+        newPlaceVC.savePlace()
         tableView.reloadData()
     }
 
--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
     
+    var currentPlace: Place?
     var imageIsChanged = false
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
@@ -24,6 +25,8 @@ class NewPlaceViewController: UITableViewController {
         tableView.tableFooterView = UIView()
         saveButton.isEnabled = false
         placeName.addTarget(self, action: #selector(textFieldChange), for: .editingChanged)
+        
+        setupEditScreen()
 
     }
     
@@ -64,7 +67,7 @@ class NewPlaceViewController: UITableViewController {
         }
     }
     
-    func saveNewPlace() {
+    func savePlace() {
         
         var image: UIImage?
         
@@ -78,9 +81,43 @@ class NewPlaceViewController: UITableViewController {
         
         let newPlace = Place(name: placeName.text!, location: placeLocation.text, type: placeType.text, imageData: imageData)
         
-        StorageManager.saveObject(newPlace)
-        
+        if currentPlace != nil {
+            try! realm.write {
+                currentPlace?.name = newPlace.name
+                currentPlace?.location = newPlace.location
+                currentPlace?.type = newPlace.type
+                currentPlace?.imageData = newPlace.imageData
+            }
+        } else {
+            StorageManager.saveObject(newPlace)
+        }
     }
+    
+      func setupEditScreen() {
+        if currentPlace != nil {
+            
+            setupNavigationBar()
+            imageIsChanged = true
+            
+            guard let data = currentPlace?.imageData, let image = UIImage(data: data) else { return }
+            
+            placeImage.image = image
+            placeImage.contentMode = .scaleAspectFill
+            placeName.text = currentPlace?.name
+            placeLocation.text = currentPlace?.location
+            placeType.text = currentPlace?.type
+        }
+    }
+    
+    private func setupNavigationBar() {
+        if let topItem = navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+        navigationItem.leftBarButtonItem = nil
+        title = currentPlace?.name
+        saveButton.isEnabled = true
+    }
+    
     @IBAction func cancelAction(_ sender: Any) {
         dismiss(animated: true)
     }
