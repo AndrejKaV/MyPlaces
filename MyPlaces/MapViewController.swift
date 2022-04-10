57d75b4fbcfaa87a77639767cdfa484208db8dec
AndrejK <0502878107@ukr.net>
2022-04-10 10:35:25 +0300

    map view controller

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by admin on 10.04.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+    
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
